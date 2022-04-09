2c89e161cd4fb43c740c5065d28d14eb016685da
Andy Barnes <andrew.barnes@googlemail.com>
2022-04-09 19:01:24 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/imported/normalize.css";
+import "../styles/bem/style.scss";
+import "../styles/imported/animate.min.css";
+import "../styles/imported/bootstrap-grid.css";
+import "../styles/imported/glide.core.min.css";
+import "../styles/imported/glide.theme.min.css";
+import "../styles/imported/hamburger.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
