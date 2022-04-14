5905fd265e2efed05f9e144d45b7b34ca2355d78
markgaze <mark.l.gaze@gmail.com>
2022-04-14 16:57:17 +0100

    Fix TS and Prettier issues

--- pages/mortgages.tsx
+++ pages/mortgages.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Head from "next/head";
-import Layout from "../components/Layout/Layout";
-import Link from "next/link";
+import React from 'react';
+import Head from 'next/head';
+import Layout from '../components/Layout/Layout';
+import Link from 'next/link';
 
 export default function Mortgages() {
   const heroContent = (
@@ -14,14 +14,12 @@ export default function Mortgages() {
       <div className="row">
         <div className="hero__section animated fadeInUp delay-1s col-md-8">
           <p className="hero__text">
-            Securing the right mortgage for you is probably one of the biggest
-            challenges and most difficult decision you’ll make. The landscape of
-            mortgages is entirely personal and nerve-wracking and you’d be
-            forgiven for not understanding it. This is where Russell Financial
-            Solutions comes in – our team of dedicated mortgage advisers will
-            help you find the right mortgage for you. Whether you need a
-            first-time buyers’ mortgage or need assistance with remortgaging
-            your house, we are on hand to help you make those difficult
+            Securing the right mortgage for you is probably one of the biggest challenges and most
+            difficult decision you’ll make. The landscape of mortgages is entirely personal and
+            nerve-wracking and you’d be forgiven for not understanding it. This is where Russell
+            Financial Solutions comes in – our team of dedicated mortgage advisers will help you
+            find the right mortgage for you. Whether you need a first-time buyers’ mortgage or need
+            assistance with remortgaging your house, we are on hand to help you make those difficult
             decisions more understandable and much easier.
           </p>
         </div>
@@ -91,30 +89,27 @@ export default function Mortgages() {
             </ul>
             <h2>Why Russell Financial Solutions?</h2>
             <p>
-              Russell Financial Solutions offer expert guidance and help to take
-              all of the stress out of finding the right mortgage for you. Not
-              only will we help you decide on the right type, we&apos;ll also
-              help you find the most suitable deal. We have access to a range of
-              mortgages from across the market, so you can be confident the
-              mortgage we recommend is right for you. We will arrange the survey
-              and communicate with all parties involved such as estate agents
-              and solicitors until you complete. It can be as simple as you
-              would like it to be, and we can take on as much or as little as
-              you need us to.
+              Russell Financial Solutions offer expert guidance and help to take all of the stress
+              out of finding the right mortgage for you. Not only will we help you decide on the
+              right type, we&apos;ll also help you find the most suitable deal. We have access to a
+              range of mortgages from across the market, so you can be confident the mortgage we
+              recommend is right for you. We will arrange the survey and communicate with all
+              parties involved such as estate agents and solicitors until you complete. It can be as
+              simple as you would like it to be, and we can take on as much or as little as you need
+              us to.
             </p>
             <h2>From First Homes to Remortgages</h2>
             <p>
-              Our team has worked hard to create and maintain our excellent
-              reputation as trusted mortgage brokers in Rayleigh Essex and
-              beyond. From individuals, to couples, to young families, we have
-              brought together a team who genuinely want to help you find the
+              Our team has worked hard to create and maintain our excellent reputation as trusted
+              mortgage brokers in Rayleigh Essex and beyond. From individuals, to couples, to young
+              families, we have brought together a team who genuinely want to help you find the
               right mortgage solution for you.
             </p>
             <h2>Current offers</h2>
             <p>
               <strong>
-                Police, NHS, Fire Service, Teachers and Armed Forces receive a
-                FREE no fee service as a thank you
+                Police, NHS, Fire Service, Teachers and Armed Forces receive a FREE no fee service
+                as a thank you
               </strong>
               . Find out more on our{" "}
               <Link href="/offers">
@@ -125,10 +120,9 @@ export default function Mortgages() {
 
             <h2>Contact Our Mortgage Advisers Today</h2>
             <p>
-              Contact Russell Financial Solutions in Rayleigh today to receive
-              our expert guidance and mortgage advice. Our specialist mortgage
-              brokers are on hand to make finding the right mortgage for you
-              efficient, quick, and stress-free.
+              Contact Russell Financial Solutions in Rayleigh today to receive our expert guidance
+              and mortgage advice. Our specialist mortgage brokers are on hand to make finding the
+              right mortgage for you efficient, quick, and stress-free.
             </p>
             <p>
               <Link href="/get-in-touch/">
