f652ec7a01851c847a452a5a4e84c634476429a1
Andy Barnes <andrew.barnes@googlemail.com>
2022-04-17 17:16:33 +0100

    Made Tawk work on load

---
+++ lib/tawk.ts
@@ -0,0 +1,14 @@
+const Tawk = () => {
+  var Tawk_API = Tawk_API || {};
+  (function () {
+    var s1 = document.createElement('script'),
+      s0 = document.getElementsByTagName('script')[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5e258b068e78b86ed8aa286b/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+};
+
+export default Tawk;
