3f477a4f3d9eff8a48ca1fe2e2cb45c1451df680
Andy Barnes <andrew.barnes102@googlemail.com>
2022-04-24 15:02:50 +0100

    Added axe to Playwright

--- tests/percy.spec.ts
+++ tests/percy.spec.ts
@@ -1,7 +1,20 @@
 import percySnapshot from '@percy/playwright';
 import test from '@playwright/test';
+import { injectAxe, checkA11y } from 'axe-playwright';
 
 test('snapshot home page', async ({ page }) => {
   await page.goto('http://localhost:3000', { waitUntil: 'networkidle' });
+
+  // stop animations from providing flaky results
+  page.addStyleTag({ content: '* { animation-duration: 1ms !important }' });
+
+  // accessibility check
+  await injectAxe(page);
+  await checkA11y(page, null, {
+    detailedReport: true,
+    detailedReportOptions: { html: true },
+    axeOptions: undefined,
+  });
+
   await percySnapshot(page, 'Example Site');
 });
